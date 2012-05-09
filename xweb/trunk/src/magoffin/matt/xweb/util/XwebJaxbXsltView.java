e3ec3841e6b04fc3b1dc493afa12545bdbf502cf
msqr <msqr@22d1487a-873b-0410-84d3-df72c8e51343>
2012-05-09 08:55:40 +0000

    Fix NPE.

--- xweb/trunk/src/magoffin/matt/xweb/util/XwebJaxbXsltView.java
+++ xweb/trunk/src/magoffin/matt/xweb/util/XwebJaxbXsltView.java
@@ -202,6 +202,9 @@ public class XwebJaxbXsltView extends XsltView implements InitializingBean {
 		} else {
 			xslt = getServletContext().getResource(getUrl());
 		}
+		if ( xslt == null ) {
+			return false;
+		}
 		try {
 			if ( !new UrlResource(xslt).getFile().exists() ) {
 				return false;
